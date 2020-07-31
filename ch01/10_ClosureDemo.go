58e453a3f73cbdc91dd78dc19902b77294ae82b4
caof <caof@zendaiproperty.com>
2020-07-31 17:23:22 +0800

    eighth

---
+++ ch01/10_ClosureDemo.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+func main(){
+	//由于没有用到闭包，所以这里的sum没有实现累加计数，导致每次sum调用均被初始化了
+
+	for i:=0;i<5;i++{
+		fmt.Printf("i=%d \t",i)
+		fmt.Println(add2(i))
+	}
+}
+
+func add2(i int) int{
+	sum:=0
+	sum+=i
+	return sum
+}
