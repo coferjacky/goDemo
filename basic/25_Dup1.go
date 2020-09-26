fdd79e77097d7bcc1ec1ed2d0ce743659667bfd5
h jackbual <22878236@qq.com>
2020-09-26 18:29:09 +0800

    update flag

--- basic/25_Dup1.go
+++ basic/25_Dup1.go
@@ -9,9 +9,14 @@ import (
 func main() {
 	//打印标准输入中多次出现的行，以重复次数开头
 	counts := make(map[string]int)
+
+	//定义变量bufio.Scanner类型变量 input
 	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
+	for input.Scan() { //每次调用input.Scan()，即读入下一行，并移除行末的换行符
 		counts[input.Text()]++
+		if input.Text() == "bye" {
+			break
+		}
 	}
 	for line, n := range counts {
 		if n > 1 {
