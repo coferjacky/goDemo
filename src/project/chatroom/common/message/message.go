025983db08c70771b53f201aff8aee9e744fcb07
caof <caof@zendaiproperty.com>
2020-12-29 13:45:29 +0800

    update chatroom

---
+++ src/project/chatroom/common/message/message.go
@@ -0,0 +1,24 @@
+package message
+
+const (
+	LoginMesType     = "LoginMes"
+	LoginResMessType = "LoginResMes"
+)
+
+type Message struct {
+	Type string `json:"type"` //消息类型
+	Data string `json:"data""`
+}
+
+//定义两个消息，后面有需要再增加
+
+type LoginMes struct {
+	UserId   int    `json:"userId"`
+	UserPwd  string `json:"userPwd"`
+	UserName string `json:"userName"`
+}
+
+type LoginResMes struct {
+	Code  int    `json:"code"`  //返回状态 500 表示改用户没有注册  200 登录成功
+	Error string `json:"error"` //返回错误信息
+}
